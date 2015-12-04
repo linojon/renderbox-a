26940e77a80439016e5e013b63b574b8532a4b83
Jonathan Linowes <jonathan@parkerhill.com>
2015-12-03 21:50:33 -0500

    new project

---
+++ app/src/main/java/com/cardbookvr/solarsystem/MainActivity.java
@@ -0,0 +1,25 @@
+package com.cardbookvr.solarsystem;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
+
+public class MainActivity extends CardboardActivity {
+    private static final String TAG = "SolarSystem";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CardboardView cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
+        cardboardView.setRestoreGLStateEnabled(false);
+        cardboardView.setRenderer(this);
+        setCardboardView(cardboardView);
+    }
+
+    
+}
